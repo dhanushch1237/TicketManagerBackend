5d4d9e6c04f295434cd39d940424dec781eeabcd
dhanushch1237 <tiru.chilakala.bilvantis.io>
2025-09-25 16:45:19 +0530

    Backend

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const corsOptions = {
     "http://localhost:4173",
     "http://127.0.0.1:5173",
     "http://127.0.0.1:4173",
-    "https://ticketmanagerbackend.onrender.com",
+    "https://ticket-marketplace-a-vrnh.bolt.host",
     /^https:\/\/.*\.bolt\.new$/,
   ],
   credentials: true,
